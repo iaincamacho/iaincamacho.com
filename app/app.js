fcda6361405cabe8390b77f2414191ad8735a5cb
Iain Camacho <icamacho@desk.com>
2014-01-21 14:15:23 -0800

    Rebuilt the app to use engine.js and not grunt

--- app/app.js
+++ app/app.js
@@ -1,9 +1,15 @@
 
-var connect = require('connect'),
-  path = require('path');
+// var connect = require('connect'),
+//   path = require('path');
 
-connect.createServer(
-  connect.static(path.join(__dirname, 'public'))
-).listen(8888, function() {
-  console.log("Server listening");
-});
+// connect.createServer(
+//   connect.static(path.join(__dirname, 'public'))
+// ).listen(8888, function() {
+//   console.log("Server listening");
+// });
+
+
+var express = require('express');
+var app = express();
+app.use('/', express.static(__dirname + '/public'));
+app.listen(5000, function() { console.log('listening')});
