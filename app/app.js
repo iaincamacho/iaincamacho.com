d0a5ce509ec673da174bf5ff63c5f144e8aeae41
Iain Camacho <icamacho@desk.com>
2014-01-21 14:43:05 -0800

    changed port to variable

--- app/app.js
+++ app/app.js
@@ -1,15 +1,9 @@
 
-// var connect = require('connect'),
-//   path = require('path');
-
-// connect.createServer(
-//   connect.static(path.join(__dirname, 'public'))
-// ).listen(8888, function() {
-//   console.log("Server listening");
-// });
-
-
 var express = require('express');
+
 var app = express();
+
+var port = process.env.PORT || 5000;
+
 app.use('/', express.static(__dirname + '/public'));
-app.listen(5000, function() { console.log('listening')});
+app.listen(port, function() { console.log('listening')});
